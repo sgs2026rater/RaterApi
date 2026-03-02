243fcd38a3936e44118697a18dde84ef9d9b6ccc
altafnpatel@gmail.com <altafnpatel@gmail.com>
2026-03-02 12:18:56 +0530

    Rating factor

--- src/Hiscox.RaterApiWrapper.Domain/Entities/RatingFactor.cs
+++ src/Hiscox.RaterApiWrapper.Domain/Entities/RatingFactor.cs
@@ -16,9 +16,9 @@ public class RatingFactor
     public decimal RecommendedCor { get; set; }
     public string RecommendedConcern { get; set; } = string.Empty;
     public List<int>? ClaimHistoryQuestions { get; set; }
-    public List<RatingFactorSectionDetails>? ClaimHistoryRatingFactorDetails { get; set; }
+    public RatingFactorSectionDetails? ClaimHistoryRatingFactorDetails { get; set; }
     public Dictionary<int, bool?>? RiskProfileQuestions { get; set; }
-    public List<RatingFactorSectionDetails>? RiskProfileRatingFactorDetails { get; set; }
+    public RatingFactorSectionDetails? RiskProfileRatingFactorDetails { get; set; }
     public int ComplexityOfRiskQuestions { get; set; }
-    public List<RatingFactorSectionDetails>? ComplexityOfRiskRatingFactorDetails { get; set; }
+    public RatingFactorSectionDetails? ComplexityOfRiskRatingFactorDetails { get; set; }
 }
