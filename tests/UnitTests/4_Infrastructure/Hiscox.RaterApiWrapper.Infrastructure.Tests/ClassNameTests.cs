b73bbf85b214247d9f2de4fa9f8d3a6ef08aadf7
Rajath H R <Rajath.HR@sutherlandglobal.com>
2026-03-04 08:16:04 +0530

    Added Unit Tests Folder Structure and Projects and Skeleton Sample Test Classes and few Skeleton Sample Unit Tests.

---
+++ tests/UnitTests/4_Infrastructure/Hiscox.RaterApiWrapper.Infrastructure.Tests/ClassNameTests.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Hiscox Insurance. All rights reserved.
+
+using Hiscox.RaterApiWrapper.Infrastructure;
+
+namespace Hiscox.RaterApiWrapper.Infrastructure.Tests;
+
+public class ClassNameTests
+{
+    #region MethodName
+
+    [Fact]
+    public void MethodNameTest_Scenario_Expectation()
+    {
+        // Arrange
+
+
+        // Act
+
+
+        // Assert
+
+    }
+
+    #endregion MethodName
+}
