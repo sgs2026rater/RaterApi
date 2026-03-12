cbf23a730147f43103aa6d8539963bb205e6de71
altafnpatel@gmail.com <altafnpatel@gmail.com>
2026-03-12 10:49:45 +0530

    Remove not required object

--- src/Hiscox.RaterApiWrapper.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Hiscox.RaterApiWrapper.Infrastructure/Data/ApplicationDbContext.cs
@@ -37,7 +37,6 @@ public class ApplicationDbContext : DbContext
     public DbSet<RatingFactorMaster>? RatingFactor { get; set; } = null;
     public DbSet<FormEligibility>? FormEligibilities { get; set; } = null;
     public DbSet<LimitRetentionFactor>? LimitRetentionFactors { get; set; } = null;
-    public DbSet<OccLimitFactor>? OccLimitFactors { get; set; } = null;
     public DbSet<ProjectTypeFactor>? ProjectTypeFactors { get; set; } = null;
     public DbSet<RetainedValueFactor>? RetainedValueFactors { get; set; } = null;
     public DbSet<RetainedValueFactorMatrix>? RetainedValueFactorMatrixs { get; set; } = null;
--- src/Hiscox.RaterApiWrapper.Infrastructure/Data/Scripts/DropTables.sql
+++ src/Hiscox.RaterApiWrapper.Infrastructure/Data/Scripts/DropTables.sql
@@ -13,7 +13,6 @@ drop TABLE IndustryForm;
 drop TABLE IndustryFormMaster;
 drop TABLE ProjectTypeFactor;
 drop TABLE LimitRetentionFactor;
-drop TABLE OccLimitFactor;
 drop TABLE RetainedValueFactor;
 drop TABLE RetainedValueFactorMatrix;
 drop TABLE RevenueBaseRate;
