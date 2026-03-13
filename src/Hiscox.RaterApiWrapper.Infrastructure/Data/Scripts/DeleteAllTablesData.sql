6e74bceb91e3d72332ca95ebf7d468013b44f95c
Rajath0818 <rajathhr.github@gmail.com>
2026-03-13 21:13:36 +0530

    Added InsertAllTablesData and DeleteAllTablesData scripts. Renamed some scripts to match the naming convention in the other scripts.

---
+++ src/Hiscox.RaterApiWrapper.Infrastructure/Data/Scripts/DeleteAllTablesData.sql
@@ -0,0 +1,31 @@
+﻿DELETE FROM FormEligibility;
+DELETE FROM Form;
+
+DELETE FROM PolicyDetails;
+DELETE FROM IndustrySpecialty;
+DELETE FROM IndustrySubSector;
+DELETE FROM IndustrySector;
+
+DELETE FROM GeographicMod;
+DELETE FROM RatingFactorMaster;
+DELETE FROM FormInfoLookup;
+--DELETE FROM IndustryForm;
+--DELETE FROM IndustryFormMaster;
+DELETE FROM ProjectTypeFactor;
+DELETE FROM LimitRetentionFactor;
+DELETE FROM RetainedValueFactor;
+DELETE FROM RetainedValueFactorMatrix;
+DELETE FROM RevenueBaseRate;
+DELETE FROM OptionalCoverageFactor;
+DELETE FROM IncludedCoverageEnhancements;
+DELETE FROM OptCovTable1;
+DELETE FROM OptionalCoverageTable1;
+DELETE FROM DisplayedDefaultPerils;
+DELETE FROM DataValidations;
+DELETE FROM OptionalCoveragesTable2s;
+DELETE FROM BusinessSizeDefinition;
+DELETE FROM RatingFactorSectionEnability;
+DELETE FROM IndustryModifier;
+DELETE FROM OptionalAdditionalCoverageFactor;
+
+DELETE FROM __EFMigrationsHistory;
